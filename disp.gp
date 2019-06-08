249131eade59e312e903612fa0d804a478d7d76f
Karl <x>
2019-06-08 15:01:45 +0200

    formatting gnuplot

--- disp.gp
+++ disp.gp
@@ -1,4 +1,8 @@
 # Gnuplot script to display the results of mem-monitor.sh
 set key autotitle columnhead
 set datafile separator " "
-plot 'test.txt'
+set grid
+set xlabel "Time (ms)"
+set ylabel "Memory (KB)"
+
+plot 'test.txt' w linespoints title "RES-MEM"
--- memtest.lisp
+++ memtest.lisp
@@ -3,7 +3,7 @@
 
 (sb-proc:run-prog "/bin/sh" "/home/modi/repos/mem-monitor/mem-monitor.sh")
 
-(loop for mem in '(2 10 5 15 3 18 13 1) do
+(loop for mem in '(2 10 5 15 3) do
   (let ((arr nil))
     (setf arr (make-array (* mem 1000000) :element-type 'double-float
 					  :initial-element 0d0))
