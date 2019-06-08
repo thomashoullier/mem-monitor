dc4226ef1dff44cadebe720cfc61e055dcf3b54e
Karl <x>
2019-06-08 14:55:19 +0200

    script can run from within SBCL

--- memtest.lisp
+++ memtest.lisp
@@ -1,6 +1,9 @@
 ;;;; Simply grab more or less memory during a given time window
+(asdf:load-system :sb-proc)
 
-(loop for mem in '(2 10 5 15) do
+(sb-proc:run-prog "/bin/sh" "/home/modi/repos/mem-monitor/mem-monitor.sh")
+
+(loop for mem in '(2 10 5 15 3 18 13 1) do
   (let ((arr nil))
     (setf arr (make-array (* mem 1000000) :element-type 'double-float
 					  :initial-element 0d0))
@@ -9,4 +12,4 @@
     (sleep 0.1))
       (sb-ext:gc :full t))
 
-(sleep 1)
+(format t "DONE~%")
